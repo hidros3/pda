65810761e911e2f408a06678bf3efeb8d9fa8ce5
taekjoo <taekjoo@taekjoo-2.local>
2015-02-01 23:23:08 +0900

    init
    init commit for python for data analysis

---
+++ pda.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+import pandas as pd
+import numpy as np
+get_ipython().magic(u'matplotlib inline')
+data
+data = [[0.98,0.889,0.987],[0.997,0.87765,0.76565]]
+data
+data*10
+data = ([0.98,0.889,0.987],[0.997,0.87765,0.76565])
+data*10
+data = {[0.98,0.889,0.987],[0.997,0.87765,0.76565]}
+data = [[0.98,0.889,0.987],[0.997,0.87765,0.76565]]
+data*10
+data = np.array(data)
